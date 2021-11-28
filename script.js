25ee69822c082f8730cf5b00bf55c636cd241002
cryptososso <cryptososso@gmail.com>
2021-11-28 21:32:16 +1200

    button command

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-document.querySelector('.main-btn').addEventListener('click', function(){
+
+document.querySelector('.menu-btn').addEventListener('click', function(){
     document.querySelector('.main-menu').classList.toggle('show');
 });
